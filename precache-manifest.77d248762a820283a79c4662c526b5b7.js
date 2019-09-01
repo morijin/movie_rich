73c8285a404b2ee1e2ab961dca68822d0c4d4740
rich <jiki.mori@gmail.com>
2019-09-02 01:23:08 +0900

    Updates

---
+++ precache-manifest.77d248762a820283a79c4662c526b5b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fcd815bb44f90307a729c4fb3c9f51",
+    "url": "/movie_rich/index.html"
+  },
+  {
+    "revision": "afa61ff7d0f6ee86e3e9",
+    "url": "/movie_rich/static/css/main.8a72977c.chunk.css"
+  },
+  {
+    "revision": "787fca61b28998d76fcc",
+    "url": "/movie_rich/static/js/2.0f3b60aa.chunk.js"
+  },
+  {
+    "revision": "afa61ff7d0f6ee86e3e9",
+    "url": "/movie_rich/static/js/main.4862a95f.chunk.js"
+  },
+  {
+    "revision": "a75d99716f50c88c3156",
+    "url": "/movie_rich/static/js/runtime~main.aa6f4028.js"
+  }
+]);
